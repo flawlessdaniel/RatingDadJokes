a67eb683c3f663eb6ce1e997fc185cd7bdcf4691
Daniel Carbajal Fernandez <dcarbajalfernandez@gmail.com>
2023-08-30 02:16:07 -0500

    Updated Web Project
    rating component.
    core project + dtos.

---
+++ RatingDadJokes/RatingDadJokes.Core/JokesDto.cs
@@ -0,0 +1,5 @@
+﻿namespace RatingDadJokes.Core
+{
+    public record Joke(string Id, string Setup, string Punchline, string Type);
+    public record Rating(Joke TargetJoke, int RatingValue);
+}
---
+++ RatingDadJokes/RatingDadJokesWeb/Components/JokeRating.razor.cs
@@ -0,0 +1,21 @@
+﻿using RatingDadJokes.Core;
+
+namespace RatingDadJokesWeb.Components
+{
+    public partial class JokeRating
+    {
+        public int rating { get; set; }
+        public Joke TargetJoke { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            TargetJoke = new Joke("123", "afasf", "gsdgsdgsd", "fsdf");
+            return Task.CompletedTask;
+        }
+
+        public async Task SendRating(int rating)
+        {
+            //TODO Send rating to the api
+        }
+    }
+}
